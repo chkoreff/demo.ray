d4000211564deb27a4c17c037d82a4de142d1f86
Patrick Chkoreff <pc@fexl.com>
2023-02-01 11:30:55 -0500

    Clean up ext.c.
    I pruned some of the comments there and also changed the indentation scheme.
    I did that because I'm about to generate this code automatically from a Fexl
    program and I wanted a very specific look.

--- src/ext.c
+++ src/ext.c
@@ -1,106 +1,115 @@
-
-/*******************************************************************************************
-*
-*   raylib [core] example - 3d camera first person
-*
-*   Example originally created with raylib 1.3, last time updated with raylib 1.3
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2015-2022 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+// raylib [core] example - 3d camera first person
+// Copyright (c) 2015-2022 Ramon Santamaria (@raysan5)
 
 #include "raylib.h"
 #include <ext.h>
-#include "pthread.h"                        // POSIX style threads management
+#include "pthread.h" // POSIX style threads management
 
 #define MAX_COLUMNS 20
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 void ray_win_display(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-    Camera camera = { 0 };
-    
-    // Generates some random columns
-    float heights[MAX_COLUMNS] = { 0 };
-    Vector3 positions[MAX_COLUMNS] = { 0 };
-    Color colors[MAX_COLUMNS] = { 0 };    
-    
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
+	{
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+	Camera camera = { 0 };
 
-    // Define the camera to look into our 3d world (position, target, up vector)
-    camera.position = (Vector3){ 4.0f, 2.0f, 4.0f };
-    camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
+	// Generates some random columns
+	float heights[MAX_COLUMNS] = { 0 };
+	Vector3 positions[MAX_COLUMNS] = { 0 };
+	Color colors[MAX_COLUMNS] = { 0 };
 
-    for (int i = 0; i < MAX_COLUMNS; i++)
-    {
-        heights[i] = (float)GetRandomValue(1, 12);
-        positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i]/2.0f, (float)GetRandomValue(-15, 15) };
-        colors[i] = (Color){ GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255 };
-    }
+	InitWindow(screenWidth, screenHeight,
+	"raylib [core] example - 3d camera first person"
+	);
 
-    SetCameraMode(camera, CAMERA_FIRST_PERSON); // Set a first person camera mode
+	// Define the camera to look into our 3d world (position, target, up vector)
+	camera.position = (Vector3){ 4.0f, 2.0f, 4.0f };
+	camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+	camera.fovy = 60.0f;
+	camera.projection = CAMERA_PERSPECTIVE;
 
-    SetTargetFPS(60);                           // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+	for (int i = 0; i < MAX_COLUMNS; i++)
+		{
+		heights[i] = (float)GetRandomValue(1, 12);
 
-    // Main game loop
-    while (!WindowShouldClose())                // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera);
-        //----------------------------------------------------------------------------------
+		positions[i] =
+			(Vector3)
+			{
+			(float)GetRandomValue(-15, 15),
+			heights[i]/2.0f,
+			(float)GetRandomValue(-15, 15)
+			};
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+		colors[i] = (Color)
+			{
+			GetRandomValue(20, 255),
+			GetRandomValue(10, 55),
+			30, 255
+			};
+		}
 
-            ClearBackground(RAYWHITE);
+	// Set a first person camera mode
+	SetCameraMode(camera, CAMERA_FIRST_PERSON);
 
-            BeginMode3D(camera);
+	SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
-                DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
-                DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
-                DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
-                DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
+	// Main game loop
+	while (!WindowShouldClose()) // Detect window close button or ESC key
+		{
+		UpdateCamera(&camera);
 
-                // Draw some cubes around
-                for (int i = 0; i < MAX_COLUMNS; i++)
-                {
-                    DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-                    DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
-                }
+		// Draw
+		BeginDrawing();
 
-            EndMode3D();
+		ClearBackground(RAYWHITE);
 
-            DrawRectangle( 10, 10, 220, 70, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 220, 70, BLUE);
+		BeginMode3D(camera);
 
-            DrawText("First person camera default controls:", 20, 20, 10, BLACK);
-            DrawText("- Move with keys: W, A, S, D", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse move to look around", 40, 60, 10, DARKGRAY);
+		// Draw ground
+		DrawPlane(
+			(Vector3){ 0.0f, 0.0f, 0.0f },
+			(Vector2){ 32.0f, 32.0f },
+			LIGHTGRAY
+			);
 
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
+		// Draw a blue wall
+		DrawCube(
+			(Vector3){ -16.0f, 2.5f, 0.0f },
+			1.0f, 5.0f, 32.0f, BLUE
+			);
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+		// Draw a green wall
+		DrawCube(
+			(Vector3){ 16.0f, 2.5f, 0.0f },
+			1.0f, 5.0f, 32.0f, LIME
+			);
 
-    return;
-}
+		// Draw a yellow wall
+		DrawCube(
+			(Vector3){ 0.0f, 2.5f, 16.0f },
+			32.0f, 5.0f, 1.0f, GOLD
+			);
 
+		// Draw some cubes around
+		for (int i = 0; i < MAX_COLUMNS; i++)
+			{
+			DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+			DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+			}
+
+		EndMode3D();
+
+		DrawRectangle( 10, 10, 220, 70, Fade(SKYBLUE, 0.5f));
+		DrawRectangleLines( 10, 10, 220, 70, BLUE);
+
+		DrawText("First person camera default controls:", 20, 20, 10, BLACK);
+		DrawText("- Move with keys: W, A, S, D", 40, 40, 10, DARKGRAY);
+		DrawText("- Mouse move to look around", 40, 60, 10, DARKGRAY);
+
+		EndDrawing();
+		}
+
+	CloseWindow(); // Close window and OpenGL context
+	return;
+	}
